c9547e0ab012857ed26ccb2ee6275503970cc42b
maxchaplin0610 <maxchaplin5@gmail.com>
2023-02-05 20:15:27 +0000

    Initial Commit

---
+++ sigmaOS/sigmaOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  sigmaOSApp.swift
+//  sigmaOS
+//
+//  Created by Max Chaplin on 05/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct sigmaOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
